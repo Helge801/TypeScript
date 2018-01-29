ac367edb15c103d4b033899a5e82cb1cfb3b4439
Bryan Lund <bryan.helge@gmail.com>
2018-01-29 14:29:30 -0700

    let vs var

---
+++ 006_vairables.ts
@@ -0,0 +1,21 @@
+// var fullName : string = "Bryan Lund";
+// let paidAccount : boolean = true;
+// const versionNumber : number = 1.3;
+
+// fullName = "Other Guy";
+// paidAccount = false;
+
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+
+function printName(f,l) {
+  var greeting : string = "Hi there, ";
+  console.log(greeting + f + " " + l);
+
+  var greeting : string = "Hey there, ";
+  console.log(greeting + f + " " + l);
+}
+
+
+printName("Bryan", "Lund");
