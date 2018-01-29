ec7228348cae0526a83a6b31dd7941e96b152b90
Bryan Lund <bryan.helge@gmail.com>
2018-01-29 15:13:42 -0700

    data types

---
+++ 007_types.ts
@@ -0,0 +1,28 @@
+// Boolean
+let paidAccount : boolean = false;
+
+// Number
+let age : number = 33;
+var taxRate : number = 7.5;
+
+// String
+var fullName : string = "Bryan Lund";
+
+// Array
+var ages : number [] = [33, 28, 11];
+
+// Tuple
+let Player : [number, string];
+player = [3, 'Corarra'];
+
+// Enum
+enum Stage {Approved, Pending, Rejected};
+let Job : Stage = Stage.Pending
+
+// Any
+var apiData : any [] = [123, "Bryan Lund", false];
+
+// Void
+function printOut(msg: string) : void {
+  console.log(msg);
+}
