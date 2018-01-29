73f03730c9ee51b0be9b3c455c5a4d06e12d94b7
Bryan Lund <bryan.helge@gmail.com>
2018-01-29 15:39:00 -0700

    union types

---
+++ 009_union_types.js
@@ -0,0 +1,10 @@
+var players_one = ["Bryan", "Lund"];
+var playerNumbers_one = [22, 88];
+console.log(players_one);
+console.log(playerNumbers_one);
+var names;
+names = ["Bryan", "Lund"];
+console.log(names);
+names = "John";
+console.log(names);
+//# sourceMappingURL=009_union_types.js.map
---
+++ 009_union_types.ts
@@ -0,0 +1,11 @@
+type PlayerArrayOne = Array<string|number>;
+let players_one : PlayerArrayOne = ["Bryan","Lund"];
+let playerNumbers_one : PlayerArrayOne = [22,88];
+console.log(players_one);
+console.log(playerNumbers_one);
+
+var names : string[]|string;
+names = ["Bryan", "Lund"];
+console.log(names);
+names = "John";
+console.log(names);
