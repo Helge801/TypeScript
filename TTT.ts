08f644d574b0ac16e9efe5abc7db5e087b5c8ec4
Bryan Lund <bryan.helge@gmail.com>
2018-01-31 14:34:35 -0700

    name spaces

--- TTT.ts
+++ TTT.ts
@@ -1,11 +1,19 @@
-// var gameBoard : string[] = [" "," "," "," "," "," "," "," "," ",];
+var gameBoard : string[] = [" "," "," "," "," "," "," "," "," ",];
 
-// var winningMethods : number[][] = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+var winningMethods : number[][] = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
-// function didWin(player : string) : boolean {
-//   for (subArr : number[] of winningMethods) {
-    
-//   }
-
-//   return true;
-// }
+function didWin(player : string) : string {
+  let won : string = "none";
+  for (let subArr of winningMethods) {
+    let count : number = 0;
+    for (let i of subArr) {
+      if (gameBoard[i] == player) {
+        count++;
+      }
+    }
+    if (count == 3){
+      won = player;
+    }
+  }
+  return won;
+}
