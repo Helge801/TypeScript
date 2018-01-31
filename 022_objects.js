20d929b0a9196138259fff7338bc2871c2962491
Bryan Lund <bryan.helge@gmail.com>
2018-01-31 13:20:20 -0700

    objects

---
+++ 022_objects.js
@@ -0,0 +1,13 @@
+var realUserObject = {
+    email: 'test@test.com',
+    firstName: 'Bryan',
+    lastName: 'Lund',
+    sayHi: function () {
+        return "Hey there!";
+    }
+};
+console.log(realUserObject.email);
+console.log(realUserObject.firstName);
+console.log(realUserObject.lastName);
+console.log(realUserObject.sayHi());
+//# sourceMappingURL=022_objects.js.map
---
+++ TTT.js
@@ -0,0 +1,5 @@
+var gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " ",];
+var winningMethods = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+function didWin(player) {
+}
+//# sourceMappingURL=TTT.js.map
