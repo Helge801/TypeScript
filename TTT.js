d59981fede1c112f76cd55637f38ff956fd32bb3
Bryan Lund <bryan.helge@gmail.com>
2018-01-31 21:02:19 -0700

    TickTackToe

--- TTT.js
+++ TTT.js
@@ -1,12 +1,11 @@
+"use strict";
 var gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " ",];
 var winningMethods = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 function didWin(player) {
-    var won = "none";
-    for (var _i = 0, winningMethods_1 = winningMethods; _i < winningMethods_1.length; _i++) {
-        var subArr = winningMethods_1[_i];
-        var count = 0;
-        for (var _a = 0, subArr_1 = subArr; _a < subArr_1.length; _a++) {
-            var i = subArr_1[_a];
+    let won = "none";
+    for (let subArr of winningMethods) {
+        let count = 0;
+        for (let i of subArr) {
             if (gameBoard[i] == player) {
                 count++;
             }
@@ -18,12 +17,10 @@ function didWin(player) {
     return won;
 }
 function canWin(player) {
-    for (var _i = 0, winningMethods_2 = winningMethods; _i < winningMethods_2.length; _i++) {
-        var subArr = winningMethods_2[_i];
-        var count = 0;
-        var used = 0;
-        for (var _a = 0, subArr_2 = subArr; _a < subArr_2.length; _a++) {
-            var i = subArr_2[_a];
+    for (let subArr of winningMethods) {
+        let count = 0;
+        let used = 0;
+        for (let i of subArr) {
             if (gameBoard[i] == player) {
                 count++;
                 used++;
@@ -38,7 +35,98 @@ function canWin(player) {
     }
     return null;
 }
-// function getOpenSpaces() : number[] {
-//   for (let )
-// } 
+function getOpenSpaces() {
+    let openSpaces = [];
+    let space = 0;
+    while (space < gameBoard.length) {
+        if (gameBoard[space] == " ") {
+            openSpaces.push(space);
+        }
+        space++;
+    }
+    return openSpaces;
+}
+function getOpenSpace(spaces) {
+    let space = 0;
+    while (space < spaces.length) {
+        if (gameBoard[spaces[space]] == " ") {
+            return spaces[space];
+        }
+        space++;
+    }
+}
+function printBoard() {
+    console.clear();
+    printLine(gameBoard.slice(0, 3));
+    printDivider();
+    printLine(gameBoard.slice(3, 6));
+    printDivider();
+    printLine(gameBoard.slice(6, 9));
+}
+function printLine(line) {
+    console.log(" " + line[0] + " | " + line[1] + " | " + line[2]);
+}
+function printDivider() {
+    console.log("-----------");
+}
+function computerGo() {
+    if (canWin("O") != null) {
+        gameBoard[getOpenSpace(canWin("O"))] = "O";
+    }
+    else if (canWin("X") != null) {
+        gameBoard[getOpenSpace(canWin("X"))] = "O";
+    }
+    else {
+        let spaces = getOpenSpaces();
+        let length = spaces.length;
+        gameBoard[spaces[Math.floor(Math.random() * length)]] = "O";
+    }
+}
+function makeMove(move) {
+    gameBoard[move - 1] = "X";
+}
+let getInput = function () {
+    return new Promise((resolve) => {
+        let readline = require('readline');
+        let rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.question('what number: ', (answer) => {
+            resolve({ answer: answer });
+            rl.close();
+        });
+    });
+};
+function play() {
+    printBoard();
+    getInput()
+        .then((res) => {
+        if (parseInt(res.answer) < 10 && parseInt(res.answer) > 0) {
+            if (gameBoard[parseInt(res.answer) - 1] == " ") {
+                makeMove(parseInt(res.answer));
+                if (didWin("X") == "X") {
+                    printBoard();
+                    console.log("Player Wins!");
+                }
+                else {
+                    computerGo();
+                    if (didWin("O") == "O") {
+                        printBoard();
+                        console.log("Computer Wins!");
+                    }
+                    else {
+                        printBoard();
+                        play();
+                    }
+                }
+            }
+            else {
+                console.log("That space is taken");
+                play();
+            }
+        }
+    });
+}
+play();
 //# sourceMappingURL=TTT.js.map
--- TTT.ts
+++ TTT.ts
@@ -1,3 +1,5 @@
+"use strict";
+
 var gameBoard : string[] = [" "," "," "," "," "," "," "," "," ",];
 
 var winningMethods : number[][] = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
@@ -38,6 +40,110 @@ function canWin(player : string) : number[] {
   return null;
 }
 
-// function getOpenSpaces() : number[] {
-//   for (let )
-// }
+function getOpenSpaces() : number[] {
+  let openSpaces : number[] = [];
+  let space : number = 0;
+  while (space < gameBoard.length ) {
+    if (gameBoard[space] == " ") {
+      openSpaces.push(space);
+    }
+    space++;
+  }
+  return openSpaces;
+}
+
+function getOpenSpace(spaces : number[]) : number {
+  let space : number = 0
+  while (space < spaces.length) {
+    if (gameBoard[spaces[space]] == " ") {
+      return spaces[space];
+    }
+    space++;
+  }
+}
+
+function printBoard() : void {
+  console.clear();
+  printLine(gameBoard.slice(0,3));
+  printDivider();
+  printLine(gameBoard.slice(3,6));
+  printDivider();
+  printLine(gameBoard.slice(6,9));
+}
+
+function printLine(line : string[]) : void {
+  console.log(" " + line[0] + " | " + line[1] + " | " + line[2]);
+}
+
+function printDivider() : void {
+  console.log("-----------");
+}
+
+function computerGo() : void {
+  if (canWin("O") != null) {
+    gameBoard[getOpenSpace(canWin("O"))] = "O";
+  } else if (canWin("X") != null) {
+    gameBoard[getOpenSpace(canWin("X"))] = "O";
+  } else {
+    let spaces : number[] = getOpenSpaces();
+    let length : number = spaces.length;
+    gameBoard[spaces[Math.floor(Math.random() * length)]] = "O";
+  }
+}
+
+function makeMove(move : number) {
+  gameBoard[move - 1] = "X";
+}
+
+let getInput = function() : Promise<{answer : string}> {
+  return new Promise((resolve) => {
+  let readline = require('readline');
+  let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+    rl.question('what number: ', (answer) => {
+      resolve({ answer: answer });
+      rl.close();
+    });
+  });
+}
+
+
+
+function play() {
+  printBoard();
+  getInput()
+  .then((res) => {
+    if (parseInt(res.answer) < 10 && parseInt(res.answer) > 0) {
+      if (gameBoard[parseInt(res.answer) - 1] == " ") {
+        makeMove(parseInt(res.answer))
+        if (didWin("X") == "X") {
+          printBoard();
+          console.log("Player Wins!");
+        } else {
+          computerGo();
+          if (didWin("O") == "O"){
+            printBoard();
+            console.log("Computer Wins!");
+          } else {
+            printBoard();
+            play();
+          }
+
+        }
+      } else {
+        console.log("That space is taken");
+        play();
+      }
+    }
+  })
+}
+
+play();
+
+
+
+
+
